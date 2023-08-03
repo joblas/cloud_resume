b91254dde2e4c45cb49d1fda7cadd986280a930f
joe <blasj@gmail.com>
2023-08-03 11:41:38 -0700

    edit function to allow counter in the index.js file

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,18 @@ const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.visitorCount = functions.https.onRequest(async (request, response) => {
+  // Set CORS headers for preflight requests
+  // Allows GETs from origin https://cloud-resume-34b6d.web.app with the Content-Type
+  // header and caches preflight response for an 1 hour
+
+  response.set('Access-Control-Allow-Origin', 'https://cloud-resume-34b6d.web.app');
+  response.set('Access-Control-Allow-Methods', 'GET');
+
+  if (request.method !== "GET") {
+    response.status(403).send('Forbidden!');
+    return;
+  }
+
   const docRef = admin.firestore().doc("pages/home");
   const doc = await docRef.get();
 
